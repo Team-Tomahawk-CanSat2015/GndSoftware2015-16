db1325d4a88aa20e2ee6a3e6bd39f34b55cb044f
Oluwatayo Shonibare <oshoniba@ryerson.ca>
2016-06-07 13:52:28 -0400

    added 2 buttons

--- gs2015.m
+++ gs2015.m
@@ -22,7 +22,7 @@ function varargout = gs2015(varargin)
 
 % Edit the above text to modify the response to help gs2015
 
-% Last Modified by GUIDE v2.5 05-Jun-2016 20:49:25
+% Last Modified by GUIDE v2.5 06-Jun-2016 20:08:36
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -650,3 +650,20 @@ function csv_list_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+
+% --- Executes on button press in LatestImage.
+function LatestImage_Callback(hObject, eventdata, handles)
+% hObject    handle to LatestImage (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global ard;
+fprintf(ard, '+');
+
+% --- Executes on button press in Buzz.
+function Buzz_Callback(hObject, eventdata, handles)
+% hObject    handle to Buzz (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global ard;
+fprintf(ard, '@');
